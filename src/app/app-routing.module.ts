5894c299dcd0e29c8bffbd06ec7f41fb48bd7169
Brackey <tlhogi.mokoena@gmail.com>
2019-03-29 04:22:08 +0200

    added Alert report UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'alert', loadChildren: './alert/alert.module#AlertPageModule' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
 ];
 
